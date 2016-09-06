e73fdfe35328e1cc3f09e1dc6df74e59cfeb2ca6
Lee Xing <lee.xing@kineticsocial.com>
2016-09-06 16:50:10 -0400

    add nabber

---
+++ ngapp/app/header/header.js
@@ -0,0 +1,4 @@
+angular.module('yujihomo')
+  .controller('HeaderCtrl', function() {
+
+  });
--- ngapp/app/yujihomo.js
+++ ngapp/app/yujihomo.js
@@ -2,11 +2,23 @@ angular.module('yujihomo', [
   'ngAnimate',
   'ui.router'
 ])
-.config(function ($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('yujihomo', {
-        url: '',
-        abstract: true
+      url: '',
+      abstract: true,
+      views: {
+        'header@': {
+          templateUrl: 'app/header/header.tmpl.html'
+        }
+      }
+    })
+    .state('home', {
+      url:'/',
+      parent: 'yujihomo'
     });
   $urlRouterProvider.otherwise('/');
+})
+.config(function($locationProvider) {
+  $locationProvider.html5Mode(true);
 });
