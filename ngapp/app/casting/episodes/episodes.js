bf8690d614ca922b6403fad7371c65cc26679411
Lee Xing <lee.xing@kineticsocial.com>
2016-09-12 11:33:20 -0400

    add episodes section

--- ngapp/app/casting/episodes/episodes.js
+++ ngapp/app/casting/episodes/episodes.js
@@ -1,11 +1,13 @@
 (function() {
   'use strict';
 
-  var episodesCtrl = function() {
-
+  var episodesCtrl = function(viewStyle) {
+    var vm = this;
+    
+    vm.getCurrentStyle = viewStyle.getCurrentStyle;
   };
 
-  episodesCtrl.$inject = [];
+  episodesCtrl.$inject = ['viewStyle'];
 
   angular.module('yujihomo')
     .controller('episodesCtrl', episodesCtrl);
