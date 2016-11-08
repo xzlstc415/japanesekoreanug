353a642be1f92835e57fddcfbb80b673cf9a5151
Zhiliang Xing <zhiliang.xing@gmail.com>
2016-11-08 01:25:35 -0500

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -81,21 +81,17 @@ ActiveRecord::Schema.define(version: 20161104004232) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "avatar_url"
-    t.boolean  "receive_email",          default: true
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
-    t.string   "email",                  default: "",   null: false
-    t.string   "encrypted_password",     default: "",   null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,    null: false
+    t.boolean  "receive_email",      default: true
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "email",              default: "",   null: false
+    t.string   "encrypted_password", default: "",   null: false
+    t.integer  "sign_in_count",      default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "comments", "episodes"
