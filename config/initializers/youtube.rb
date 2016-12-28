4d417798feeaeee8404f4bd8da625fdb4369b93b
Zhiliang Xing <zhiliang.xing@gmail.com>
2016-12-28 18:27:15 -0500

    just for test

--- config/initializers/youtube.rb
+++ config/initializers/youtube.rb
@@ -1,6 +1,4 @@
 Yt.configure do |config|
-  config.client_id = ENV['GOOGLE_CLIENT_ID']
-  config.client_secret = ENV['GOOGLE_CLIENT_SECRET']
   config.log_level = :debug
-  config.api_key = ENV['GOOGLE_API_KEY']
+  config.api_key = 'AIzaSyCKU1-v62yODOnzlh7TpECm-0OhylDpzfg'
 end
