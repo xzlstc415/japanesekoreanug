5258a3f2cc6970cd061c88572b306a9eaf329607
Zhiliang Xing <zhiliang.xing@gmail.com>
2017-02-26 22:04:01 -0500

    update validation for end time

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,11 @@ class Event < ActiveRecord::Base
   enum event_type: [:streaming, :editing, :school, :others]
 
   validates :title, :start, :end, :event_type, presence: true
+  validate :end_is_later_than_start
+
+  private
+
+  def end_is_later_than_start
+    errors.add(:end, 'must be later than start') if self.start >= self.end
+  end
 end
