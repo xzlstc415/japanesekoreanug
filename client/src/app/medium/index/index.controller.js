48c1934e90533715d4829986e92907da919c45d0
Zhiliang Xing <zhiliang.xing@gmail.com>
2017-04-11 23:28:00 -0400

    add delete image function

--- client/src/app/medium/index/index.controller.js
+++ client/src/app/medium/index/index.controller.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  var MediumIndexController = function($state, media, Medium, usSpinnerService, ErrorMessageHandler, toastr, mediumType) {
+  var MediumIndexController = function($state, media, Medium, usSpinnerService, ErrorMessageHandler, toastr, mediumType, _) {
     var vm = this;
     vm.media = media.data;
     vm.mediumType = mediumType;
@@ -11,7 +11,8 @@
       Medium.save(vm.medium, vm.mediumFile)
         .then(function(res) {
           usSpinnerService.stop('spinner-1');
-          vm.media.push(res);
+          toastr.success('image has been uploaded successfully');
+          vm.media.push(res.data);
         })
         .catch(function(res) {
           usSpinnerService.stop('spinner-1');
@@ -19,15 +20,27 @@
         });
     };
 
-    var removeMedia = function() {
-
+    var removeMedia = function(medium) {
+      usSpinnerService.spin('spinner-1');
+      Medium.destroy(medium.id)
+        .then(function() {
+          usSpinnerService.stop('spinner-1');
+          toastr.success('image has been deleted successfully');
+          _.remove(vm.media, function(m) {
+            return m.id == medium.id;
+          });
+        })
+        .catch(function(res) {
+          usSpinnerService.stop('spinner-1');
+          ErrorMessageHandler.displayErrors(res);
+        });
     };
 
     vm.addNewMedia = addNewMedia;
     vm.removeMedia = removeMedia;
   };
 
-  MediumIndexController.$inject = ['$state', 'media', 'Medium', 'usSpinnerService', 'ErrorMessageHandler', 'toastr', 'mediumType'];
+  MediumIndexController.$inject = ['$state', 'media', 'Medium', 'usSpinnerService', 'ErrorMessageHandler', 'toastr', 'mediumType', '_'];
 
   angular.module('yujihomo')
     .controller('MediumIndexController', MediumIndexController);
