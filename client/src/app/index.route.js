beddde29801d97f99dacd0f8e4ab3d36ab5da936
Zhiliang Xing <zhiliang.xing@gmail.com>
2017-05-18 00:09:24 -0400

    clean up part of the css

--- client/src/app/index.route.js
+++ client/src/app/index.route.js
@@ -12,7 +12,7 @@
         abstract: true,
         views: {
           '@': {
-            template: "<header class='header' ui-view='header'></header><div class='container-fluid main-container-padding-btm well-lg' id='main-container' ui-view></div><footer ui-view='footer'></footer>"
+            template: "<header class='header' ui-view='header'></header><div class='container-fluid' id='main-container' ui-view></div><footer ui-view='footer'></footer>"
           },
           'header@yujihomo': {
             templateUrl: 'app/header/header.tmpl.html',
